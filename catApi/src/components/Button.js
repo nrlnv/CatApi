b7cbb14ad136d15441cee9f2253aecb99475e1f3
David Luiz <yeldos.ktl@gmail.com>
2021-02-27 23:55:08 +0500

    added breeds detail screen

---
+++ catApi/src/components/Button.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {TouchableOpacity, Text, StyleSheet} from 'react-native';
+import { scale } from '../constants/globalStyles';
+
+const Button = ({title}) => {
+  return (
+    <TouchableOpacity style={styles.container}>
+      <Text style={styles.title}>{title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fefefe',
+    padding: scale(10),
+    borderRadius: 10,
+    shadowColor: 'black',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 2,
+  },
+  title: {
+    color: 'blue',
+    fontSize: scale(15),
+    fontWeight: '600',
+  },
+});
+
+export default Button;
--- catApi/src/screens/BreedDetailScreen.js
+++ catApi/src/screens/BreedDetailScreen.js
@@ -1,13 +1,68 @@
 import React from 'react';
-import {View, Text, SafeAreaView} from 'react-native';
+import {View, Text, SafeAreaView, StyleSheet, Image} from 'react-native';
+
+import Button from '../components/Button';
+
+import {dimensions, scale} from '../constants/globalStyles';
 
 const BreedDetailScreen = ({route}) => {
   const {item} = route.params;
   return (
-    <SafeAreaView>
-      <Text>{item.name}</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.imageView}>
+        <Image source={{uri: item.image.url}} style={styles.image} />
+      </View>
+      <View style={styles.textView}>
+        <Text style={styles.textName}>{item.name}</Text>
+        <Text style={styles.textDescription}>{item.description}</Text>
+      </View>
+      <View style={styles.buttonView}>
+        <Button title="Another photo" />
+        <Button title="Add to favourites" />
+      </View>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  imageView: {
+    alignItems: 'center',
+    marginTop: scale(20),
+    shadowColor: 'black',
+    shadowOffset: {
+      width: 3,
+      height: 5,
+    },
+    shadowOpacity: 0.4,
+    shadowRadius: 10,
+  },
+  image: {
+    width: dimensions.fullWidth - scale(40),
+    height: dimensions.fullWidth - scale(40),
+    borderRadius: 15,
+  },
+  textView: {
+    marginHorizontal: scale(20),
+    marginTop: scale(20),
+  },
+  textName: {
+    fontSize: scale(20),
+    fontWeight: 'bold',
+    marginVertical: scale(15),
+  },
+  textDescription: {
+    fontSize: scale(15),
+    textAlign: 'justify',
+  },
+  buttonView: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginHorizontal: scale(20),
+    marginTop: scale(20)
+  },
+});
 
 export default BreedDetailScreen;
